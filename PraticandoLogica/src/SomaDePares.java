03303d42f6f6cddadef303c1152727b609f4c2bc
Walisson oliveira <walissonoliveira1@outlook.com>
2025-06-08 10:51:50 -0300

    Exercício de soma de pares em um array

--- PraticandoLogica/src/SomaDePares.java
+++ PraticandoLogica/src/SomaDePares.java
@@ -1,20 +1,18 @@
-public class SomaDePares {
-    public static void main(String[] args) {
-        int[] numeros = {1,2,3,4,5,6};
-        int soma = somarPares(numeros);
-        System.out.println("Soma dos pares: " + soma);
-
-    }
-
-public static int somarPares(int[] array){
-    int soma = 0;
-    for( int numero : array){
-        if (numero % 2 ==0){
-            soma+= numero;
-
+public class SomaDePares{
+    public static int somarPares(int[] numeros){
+        int soma = 0;
+        for(int numero : numeros){
+            if(numero % 2 == 0){
+                soma += numero;
+            }
         }
+        return soma;
     }
-    return soma;
-}
-    
+
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6};
+        int resultado = somarPares(array);
+        System.out.println("Soma dos pares: " + resultado);
+    }
 }
