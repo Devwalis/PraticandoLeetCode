f74d7a438cca1b2c4e793b507eac85fdaa4b4569
Walisson oliveira <walissonoliveira1@outlook.com>
2025-06-15 10:33:36 -0300

    Exercício concluido

--- PraticandoLogica/src/EncontrarNumeroUnico.java
+++ PraticandoLogica/src/EncontrarNumeroUnico.java
@@ -1,3 +1,18 @@
 public class EncontrarNumeroUnico{
-    
+    public static int encontrarNumeroUnico(int[] nums){
+     int   resultado = 0;
+
+     for(int num : nums){
+        resultado ^= num;
+
+     }
+     return resultado;
+
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4, 1, 2, 1, 2, 4, 9};
+        int numero = encontrarNumeroUnico(array);
+        System.out.println("O número único é: " + numero);
+    }
 }
