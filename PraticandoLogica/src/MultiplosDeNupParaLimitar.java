9380e0be316ece51cf772f33337635551e15500f
Walisson oliveira <walissonoliveira1@outlook.com>
2025-06-19 00:39:21 -0300

    Exercício Multiples of n up to limit

---
+++ PraticandoLogica/src/MultiplosDeNupParaLimitar.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MultiplosDeNupParaLimitar{
+
+    public static List<Integer> buscaMultiplos(int n, int limit){
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = n; i<= limit; i += n){
+            result.add(i);
+        }
+        return result;
+
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(buscaMultiplos(2, 6));
+        
+    }
+    
+}
