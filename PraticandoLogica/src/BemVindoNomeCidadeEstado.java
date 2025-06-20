841c9a211f7cb4140ee832d88d35f937fd56815f
Walisson oliveira <walissonoliveira1@outlook.com>
2025-06-20 02:13:14 -0300

    Complemento do exercício Bem vindo com nome, cidade e estado

--- PraticandoLogica/src/BemVindoNomeCidadeEstado.java
+++ PraticandoLogica/src/BemVindoNomeCidadeEstado.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class BemVindoNomeCidadeEstado{
 
 
     public static String Hello(String[] nome, String cidade, String estado){
         String fullName = String.join("", nome);
         return String.format("Olá seja %s Bem vindos %s, %s!",fullName, nome, estado);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome completo: ");
+        String nomeCompleto = scanner.nextLine();
+        String[] nome = nomeCompleto.trim().split("");
+
+        System.out.println("Digite sua cidade: ");
+        String cidade = scanner.nextLine();
+
+        System.out.println("Digite seu estado: ");
+        String estado = scanner.nextLine();
+
+        scanner.close();
     }
 }
