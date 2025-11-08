05665cabb0545ff5f1cb176c36b951b54500f0a1
Walisson <Walissonoliveira1@outlook.com>
2025-11-07 21:03:18 -0300

    Exercício de soma entre dois números

---
+++ PraticandoLogica/SomaEntreDoisNumeros.java
@@ -0,0 +1,24 @@
+
+package PraticandoLogica;
+public class SomaEntreDoisNumeros {
+
+    public static int somaDeDoisNumeros(int a, int b){
+
+
+        if(a == b){
+            return a;
+        }
+
+
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+
+        int sum = 0;
+
+        for(int i = min; i <= max; i++){
+            sum += 1;
+        }
+
+        return a;
+    }
+}
--- PraticandoLogica/src/BemVindoNomeCidadeEstado.java
+++ PraticandoLogica/src/BemVindoNomeCidadeEstado.java
@@ -1,3 +1,5 @@
+package PraticandoLogica;
+
 import java.util.Scanner;
 
 public class BemVindoNomeCidadeEstado{
