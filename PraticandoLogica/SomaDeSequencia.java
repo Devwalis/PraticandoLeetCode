93af4f3c5d75e60d2abfe41c1fec380b41d804ac
Walisson <Walissonoliveira1@outlook.com>
2025-11-08 11:22:44 -0300

    Exercício Soma de Sequência de números inteiros

---
+++ PraticandoLogica/SomaDeSequencia.java
@@ -0,0 +1,26 @@
+package PraticandoLogica;
+
+public class SomaDeSequencia {
+    
+    /*
+     * @param inicio valor inicial da sequência (não negativo)
+     * @param fim o valor final da sequência (não negativo)
+     * @param passo o incremento da sequência (não negativo)
+     * 
+     */
+
+
+     public long sequenceSum(int inicio, int fim, int passo){
+        if(inicio > fim){
+            return 0;
+
+        }
+        long sum = 0;
+
+        for(long i = inicio; i <= fim; i += passo){
+            sum += 1;
+
+        }
+        return sum;
+     }
+}
--- PraticandoLogica/src/ContarOcorrencias.java
+++ PraticandoLogica/src/ContarOcorrencias.java
@@ -23,3 +23,22 @@ public class ContarOcorrencias {
     }
     
 }
+
+
+
+/*
+ * Alternativa usando laço for tradicional
+ * 
+ * public int countOcorrencesAlternative(String str, char searchChar){
+ *  
+ * int count = 0;
+ * 
+ * for(int i = 0; i < str.length(); i++){
+ *  if(str.charAt(i) == searchChar){
+ *          count++;
+ * }
+ * }
+ * return count;
+ * 
+ * }
+ */
