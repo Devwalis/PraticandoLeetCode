7ddbdca775c74d9889bc6f749c5586708a5c3da6
Walisson <Walissonoliveira1@outlook.com>
2025-11-26 23:11:25 -0300

    adicionando praticas de vetor

--- PraticandoLogica/src/estruturaDeDados/test/TestandoVetor.java
+++ PraticandoLogica/src/estruturaDeDados/test/TestandoVetor.java
@@ -1,5 +1,9 @@
 package PraticandoLogica.src.estruturaDeDados.test;
 
+import PraticandoLogica.src.estruturaDeDados.Vetor;
+
 public class TestandoVetor {
+    Vetor vetor = new Vetor(5);
+    int i= 0;
     
 }
