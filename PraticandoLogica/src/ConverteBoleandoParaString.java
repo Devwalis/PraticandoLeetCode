c18af86f33c20094649a2ceb1fca2c400b23385a
Walisson <Walissonoliveira1@outlook.com>
2025-12-09 23:35:00 -0300

    Convertendo um boleano para String

---
+++ PraticandoLogica/src/ConverteBoleandoParaString.java
@@ -0,0 +1,10 @@
+package PraticandoLogica.src;
+
+public class ConverteBoleandoParaString {
+    public static String convert(boolean b){
+        if( b == true){
+            return"true";
+        }else{
+            return"false";        }
+    }
+}
