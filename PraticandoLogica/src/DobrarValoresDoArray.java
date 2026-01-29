64700c01acc5ad1283f34af9caeaa74e5aae7407
Walisson <Walissonoliveira1@outlook.com>
2026-01-29 02:30:56 -0300

    CDF do exercício 62

---
+++ PraticandoLogica/src/DobrarValoresDoArray.java
@@ -0,0 +1,14 @@
+package PraticandoLogica.src;
+
+public class DobrarValoresDoArray {
+    
+    public static int[] map(int[] arr){
+        int[] result = new int[arr.length];
+
+
+        for(int i=0; i < arr.length; i++){
+            result[i] = arr[i] * 2;
+        }
+        return result;
+    }
+}
