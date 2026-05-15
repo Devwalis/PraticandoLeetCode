1c6bd643f9f7ce0acc6e4bd1c4645bfe7fa10e8d
Walisson <Walissonoliveira1@outlook.com>
2026-05-14 23:10:33 -0300

    teste de algoritmo exercício 71

--- PraticandoLogica/src/StringTerminaCom.java
+++ PraticandoLogica/src/StringTerminaCom.java
@@ -16,3 +16,10 @@ Verificar se a string principal termina com a string de sufixo fornecida
 		return str.endsWith(ending);
 
 }
+
+	public static void main(String[] args){
+		StringEndsWith checker = new StringEndsWith();
+
+
+		System.out.println("\"abc\", \"bc\" -> " + checker.solution("abc", "bc"));
+
