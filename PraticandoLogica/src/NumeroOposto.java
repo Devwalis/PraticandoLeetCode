e35f32854d6f79e0455a43c0a7d3c718776d5b20
Walisson <Walissonoliveira1@outlook.com>
2026-05-23 08:17:28 -0300

    código do exercício 77 (número oposto)

---
+++ PraticandoLogica/src/NumeroOposto.java
@@ -0,0 +1,16 @@
+public class NumeroOposto{
+		
+	/*
+
+	Retorna o inverso aditivo (oposto) de um número dado.
+	@param number 0 número inteiro de entrada.
+	@return O número oposto
+
+	
+	*/
+
+
+	public int oposto(int number){
+		return -number;
+	}	
+	}
