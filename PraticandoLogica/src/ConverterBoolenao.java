1758b2bb46bebcc7cdcd5fef3321328b5adaeea0
Walisson <Walissonoliveira1@outlook.com>
2026-05-25 10:58:44 -0300

    Código do exercício 79

---
+++ PraticandoLogica/src/ConverterBoolenao.java
@@ -0,0 +1,21 @@
+public class ConverterBoolenao{
+	/*
+
+	Converter um valor booleano para as string "yes" ou "no"
+
+	@param bool 0 valor booleano de entrada
+	@return "yes" se o valor for true, "No" se o valor for false
+
+	*/
+	public String convert(boolean bool){
+
+	if(bool){
+		return "yes";	
+	}else{
+		return "No";
+	}
+
+
+}
+
+	}
