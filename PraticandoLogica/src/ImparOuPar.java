34501da3fb0e4d5900c5e97e7fe4ac8be6e4e706
Walisson <Walissonoliveira1@outlook.com>
2026-05-28 02:22:42 -0300

    Código do exercício 82

---
+++ PraticandoLogica/src/ImparOuPar.java
@@ -0,0 +1,23 @@
+public class ImparOuPar{
+	/*
+	Verificar se um número inteiro é par ou impar
+	
+	@param number O número inteiro é par ou impar
+	@return"Even" se o número for par, "Odd" se for impar.
+	
+	
+	*/
+
+	public String EvenOrOdd(int number){
+	if(number % 2 == 0){
+		return "Even";
+
+		
+} else{
+	return "Odd";
+	}
+		
+	
+}
+
+}
