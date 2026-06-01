a0b186a0afadd7702ff154c1e94eefb37e9f4bd2
Walisson <Walissonoliveira1@outlook.com>
2026-06-01 00:47:14 -0300

    contruindo o codigo do exercício 84

---
+++ PraticandoLogica/src/TempoDesdeaMeiaNoite.java
@@ -0,0 +1,18 @@
+public class TempoDesdeaMeiaNoite{
+
+/*
+	Converter horas, minutos e segundos decorridos desde a meia-noite para milissegundos.
+	
+
+
+	@param h horas decorridaas desde a meia-noite (0 a 23)
+	@param m minutos decorridos desde a meia-noite (0 a 59)
+	@param s Segundos decorridos desde a meia-noite (0 a 59)
+	@return 0 tempo total decorrido desde a meia noite em milissegundos
+*/
+public int horas(int h, int m, int s){
+	
+	
+	}
+
+}
