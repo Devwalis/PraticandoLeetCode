f500139e79dfbf9078168ff8ccd6239f4e9510c4
Walisson <Walissonoliveira1@outlook.com>
2026-06-02 00:39:32 -0300

    Código completo do exercício 84 (Tempo desde a meia noite)

--- PraticandoLogica/src/TempoDesdeaMeiaNoite.java
+++ PraticandoLogica/src/TempoDesdeaMeiaNoite.java
@@ -12,7 +12,23 @@ public class TempoDesdeaMeiaNoite{
 */
 public int horas(int h, int m, int s){
 	
+
+	 // 1 hora possui 3.600.milissegundos (60 min * 60 seg * 1000 ms)
+	// 1 minuto possui 60.000 milisegundos (60 seg * 1000 ms)
+	// 1 segundo possui 1.000 milissegundos
+
+	// Multiplica-se cada unidade de tempo para obter os milissegundos e soma-se o total
+	
+	return (h * 3600000) + (m * 60000) + (s * 1000);
+	
 	
 	}
 
-}
+
+	public static void main(String[] args) {
+		Clock clock = new Clock();
+	
+	 // teste com exemplo do anunciado (h = 0, m = 1, s = 1) 
+
+	System.out.println("h = 0, m = 1, s= 1 -> " + clock.horas(0 , 1,  1));
+ 
