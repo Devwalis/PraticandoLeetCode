ed63f10bbf50070876e0cb74f5791bdfa06bf9d8
Walisson <Walissonoliveira1@outlook.com>
2026-06-02 22:00:00 -0300

    COnstruindo o código do exercício 85

---
+++ PraticandoLogica/src/ExpressoesImportam.java
@@ -0,0 +1,25 @@
+public class ExpressoesImportam{
+	/*
+
+	Encontrar o maior valor possível inserindo +,  * e () entre três inteiros fornecidos.
+	
+	
+	@param a O primeiro número inteiro positivo (1 a 10)
+	@param b O segundo número inteiro positivo (1 a 10)
+	@param c O terceiro número inteiro positivo (1 a 10)
+	
+
+
+*/	
+		
+
+		public int expressao(int a, int b, int c){
+	//1. calcular todas as 6 possíveis variações de posicionamento de operadores e parênteses
+
+	
+	
+	
+	
+	}
+	
+}
