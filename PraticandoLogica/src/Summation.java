f7cfdfd1e1aa152c6890975795d0e15bea367839
Walisson <Walissonoliveira1@outlook.com>
2026-06-16 02:03:56 -0300

    Exercício 91 (summation)

---
+++ PraticandoLogica/src/Summation.java
@@ -0,0 +1,15 @@
+public class Summation{
+	public static int summation(int num){
+	// Soma de 1 a num a fórmulad da progressão aritmétrica
+	return num * (num + 1) / 2;
+	}
+	
+	
+		
+	public static void main(String[] args){
+		
+		
+		System.out.println(Summation.summation(2));
+		
+	}
+}
